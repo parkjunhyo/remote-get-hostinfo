ac46f135b287a3c2c3702d5c32da2d62551a52b7
park junhyo <junhyo.luck@gmail.com>
2018-09-28 05:28:02 +0000

    Update hosts.py

--- hosts.py
+++ hosts.py
@@ -1,21 +1,21 @@
 #! /usr/bin/env python
 TARGETHOSTS = [
  {
-   "host" : "54.180.9.144",
+   "host" : "your-ip-address",
    "port" : 22,
    "username" : "ubuntu",
    "method" : "key",
    "option" : {
-      "keyname" : "aws-seoul-junhyo2.park-kp1.pem"
+      "keyname" : "your-private.pem"
    }
  },
  {
-   "host" : "178.128.21.101",
+   "host" : "your-ip-address",
    "port" : 38300,
    "username" : "root",
    "method" : "password",
    "option" : {
-      "password" : "c23bfc19b7f259e4a0dc7e5e54@1"
+      "password" : "your-password"
    }
  }
 ]
