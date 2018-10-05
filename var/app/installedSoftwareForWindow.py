deda8cd9a5b69f3c756d99d01e92294869b6bf42
Your Name <you@example.com>
2018-10-05 03:17:49 +0000

    update

--- var/app/installedSoftwareForWindow.py
+++ var/app/installedSoftwareForWindow.py
@@ -18,6 +18,49 @@ class InstalledSoftwareForWindow:
             "installedapp":[]
         }
 
+    def __obtainWin32Computersystem(self, rdict):
+        fixed_rdict = rdict[0]
+        fixed_rdictkey = fixed_rdict.keys()
+        for kname in fixed_rdictkey:
+            if re.compile("^name",re.I).search(kname) and len(kname) == 4:
+               self.outputformat["hostname"] = fixed_rdict[kname].strip()
+               continue
+            elif re.compile("^workgroup",re.I).search(kname) and len(kname) == 9:
+               self.outputformat["osworkgroup"] = fixed_rdict[kname].strip()
+               continue
+            elif re.compile("^domain",re.I).search(kname) and len(kname) == 6:
+               self.outputformat["osdomain"] = fixed_rdict[kname].strip()
+               continue
+
+    def __obtainWin32Operatingsystem(self, rdict):
+        fixed_rdict = rdict[0]
+        fixed_rdictkey = fixed_rdict.keys()
+        for kname in fixed_rdictkey:
+            if re.compile("^caption",re.I).search(kname) and len(kname) == 7:
+               self.outputformat["osname"] = fixed_rdict[kname].strip()
+               continue
+            elif re.compile("^osarchitecture",re.I).search(kname) and len(kname) == 14:
+               self.outputformat["osarchitecture"] = fixed_rdict[kname].strip()
+               continue
+            elif re.compile("^version",re.I).search(kname) and len(kname) == 7:
+               self.outputformat["osversion"] = fixed_rdict[kname].strip()
+               continue
+
+    def __obtainWin32Product(self, rdict):
+        applist = []
+        for fixed_rdict in rdict:
+           tmpdict = {}
+           fixed_rdictkey = fixed_rdict.keys()
+           for kname in fixed_rdictkey:
+               if re.compile("^caption",re.I).search(kname) and len(kname) == 7:
+                  tmpdict["caption"] = fixed_rdict[kname].strip()
+                  continue
+               elif re.compile("^version",re.I).search(kname) and len(kname) == 7:
+                  tmpdict["version"] = fixed_rdict[kname].strip()
+                  continue
+           applist.append(tmpdict)
+        self.outputformat["installedapp"] = applist
+
     def installedSoftwareForWindow(self, targethost, WORKENV):
         # clean and make directory for result
         #ipstring = str(''.join(targethost['host'].strip().split('.')))
@@ -35,18 +78,19 @@ class InstalledSoftwareForWindow:
         self.outputformat["ipaddress"] = targethost['host'] 
         #
         rdict = wmic.query("SELECT * FROM Win32_ComputerSystem")
-        fixed_rdict = rdict[0]
-        fixed_rdictkey = fixed_rdict.keys()
-        for kname in fixed_rdictkey:
-            if re.compile("^name",re.I).search(kname) and len(kname) == 4:
-               self.outputformat["hostname"] = fixed_rdict[kname].strip()
-               continue
-            elif re.compile("^workgroup",re.I).search(kname) and len(kname) == 9:
-               self.outputformat["osworkgroup"] = fixed_rdict[kname].strip()
-               continue
-            elif re.compile("^domain",re.I).search(kname) and len(kname) == 6:
-               self.outputformat["osdomain"] = fixed_rdict[kname].strip()
-               continue
+        self.__obtainWin32Computersystem(rdict)
+        #fixed_rdict = rdict[0]
+        #fixed_rdictkey = fixed_rdict.keys()
+        #for kname in fixed_rdictkey:
+        #    if re.compile("^name",re.I).search(kname) and len(kname) == 4:
+        #       self.outputformat["hostname"] = fixed_rdict[kname].strip()
+        #       continue
+        #    elif re.compile("^workgroup",re.I).search(kname) and len(kname) == 9:
+        #       self.outputformat["osworkgroup"] = fixed_rdict[kname].strip()
+        #       continue
+        #    elif re.compile("^domain",re.I).search(kname) and len(kname) == 6:
+        #       self.outputformat["osdomain"] = fixed_rdict[kname].strip()
+        #       continue
         # clean and make directory for result
         ipstring = str(''.join(targethost['host'].strip().split('.')))
         RESULTFULLPATH = WORKENV['RESULTPATH']+"/"+targethost["ostype"]+"-"+self.outputformat["hostname"]+"-"+ipstring
@@ -55,37 +99,39 @@ class InstalledSoftwareForWindow:
         os.makedirs(RESULTFULLPATH)
         # write file for Win32_OperatingSystem
         rdict = wmic.query("SELECT * FROM Win32_OperatingSystem")
-        fixed_rdict = rdict[0]
-        fixed_rdictkey = fixed_rdict.keys()
-        for kname in fixed_rdictkey:
-            if re.compile("^caption",re.I).search(kname) and len(kname) == 7:
-               self.outputformat["osname"] = fixed_rdict[kname].strip()
-               continue
-            elif re.compile("^osarchitecture",re.I).search(kname) and len(kname) == 14:
-               self.outputformat["osarchitecture"] = fixed_rdict[kname].strip()
-               continue
-            elif re.compile("^version",re.I).search(kname) and len(kname) == 7:
-               self.outputformat["osversion"] = fixed_rdict[kname].strip()
-               continue
+        self.__obtainWin32Operatingsystem(rdict)
+        #fixed_rdict = rdict[0]
+        #fixed_rdictkey = fixed_rdict.keys()
+        #for kname in fixed_rdictkey:
+        #    if re.compile("^caption",re.I).search(kname) and len(kname) == 7:
+        #       self.outputformat["osname"] = fixed_rdict[kname].strip()
+        #       continue
+        #    elif re.compile("^osarchitecture",re.I).search(kname) and len(kname) == 14:
+        #       self.outputformat["osarchitecture"] = fixed_rdict[kname].strip()
+        #       continue
+        #    elif re.compile("^version",re.I).search(kname) and len(kname) == 7:
+        #       self.outputformat["osversion"] = fixed_rdict[kname].strip()
+        #       continue
         #fname = RESULTFULLPATH+"/Win32_OperatingSystem"
         #f = open(fname, 'w')
         #f.write(str(rdict))
         #f.close()
         # wirte file for Win32_Product
         rdict = wmic.query("SELECT * FROM Win32_Product")
-        applist = []
-        for fixed_rdict in rdict:
-           tmpdict = {}
-           fixed_rdictkey = fixed_rdict.keys()
-           for kname in fixed_rdictkey:
-               if re.compile("^caption",re.I).search(kname) and len(kname) == 7:
-                  tmpdict["caption"] = fixed_rdict[kname].strip()
-                  continue
-               elif re.compile("^version",re.I).search(kname) and len(kname) == 7:
-                  tmpdict["version"] = fixed_rdict[kname].strip()
-                  continue
-           applist.append(tmpdict)
-        self.outputformat["installedapp"] = applist
+        self.__obtainWin32Product(rdict)
+        #applist = []
+        #for fixed_rdict in rdict:
+        #   tmpdict = {}
+        #   fixed_rdictkey = fixed_rdict.keys()
+        #   for kname in fixed_rdictkey:
+        #       if re.compile("^caption",re.I).search(kname) and len(kname) == 7:
+        #          tmpdict["caption"] = fixed_rdict[kname].strip()
+        #          continue
+        #       elif re.compile("^version",re.I).search(kname) and len(kname) == 7:
+        #          tmpdict["version"] = fixed_rdict[kname].strip()
+        #          continue
+        #   applist.append(tmpdict)
+        #self.outputformat["installedapp"] = applist
         #fname = RESULTFULLPATH+"/Win32_Product"
         #f = open(fname, 'w')
         #f.write(str(rdict))
