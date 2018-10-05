bfc9ce64ff32a2028c24b9aa99ae930eb8bd1d96
Your Name <you@example.com>
2018-10-05 01:03:15 +0000

    update

--- utils/local-run-scripts/run-window.cmd
+++ utils/local-run-scripts/run-window.cmd
@@ -22,29 +22,65 @@ for /f "skip=1 tokens=1" %%i in ('wmic computersystem get name') do (
 :csnamebreak
 
 :: Set file to write
-set WF=window-%MYCSNAME%-%MYIP%.origin.txt
+set MYIP=%MYIP: =%
+set MYCSNAME=%MYCSNAME: =%
+set WF=window-%MYCSNAME%-%MYIP%.orgin.txt
+echo ----- : begin result from script : ----- > %WF%
 
-:: Run
-echo ----- : result from script : ----- > %WF%
 :: computersystem
 echo computersystem info is getting...
 echo ----- : begin computersystem : ----- >> %WF%
-for /f "delims=" %%i in ('wmic computersystem') do (
-   echo %%i >> %WF%
+echo Name=%MYCSNAME% >> %WF%
+for /f "skip=1 delims=" %%i in ('wmic computersystem get workgroup') do (
+   set GETWORKGROUP=%%i
+   goto workgroupbreak
 )
+:workgroupbreak
+echo Workgroup=%GETWORKGROUP% >> %WF%
+for /f "skip=1 delims=" %%i in ('wmic computersystem get domain') do (
+   set GETDOMAIN=%%i
+   goto domnamebreak
+)
+:domnamebreak
+echo Domain=%GETDOMAIN% >> %WF%
 echo ----- : end computersystem : ----- >> %WF%
+
 :: operatingsystem
 echo operatingsystem info is getting...
 echo ----- : begin operatingsystem : ----- >> %WF%
-for /f "delims=" %%i in ('wmic os') do (
-   echo %%i >> %WF%
+for /f "skip=1 delims=" %%i in ('wmic os get caption') do (
+   set GETCAPTION=%%i
+   goto captionbreak
 )
+:captionbreak
+echo Caption=%GETCAPTION% >> %WF%
+for /f "skip=1 delims=" %%i in ('wmic os get osarchitecture') do (
+   set GETOSARCHITECTURE=%%i
+   goto archbreak
+)
+:archbreak
+echo OSArchitecture=%GETOSARCHITECTURE% >> %WF%
+for /f "skip=1 delims=" %%i in ('wmic os get version') do (
+   set GETVERSION=%%i
+   goto versionbreak
+)
+:versionbreak
+echo version=%GETVERSION% >> %WF%
 echo ----- : end operatingsystem : ----- >> %WF%
+
+
 :: product
 echo product info is getting...
 echo ----- : begin product : ----- >> %WF%
-for /f "delims=" %%i in ('wmic product') do (
+@echo > %rfilename%
+for /f "usebackq delims=" %%i in (`wmic product get caption^,version^/format:list`) do (
+   @echo %%i >> %rfilename%
+)
+::   if you suffer from C runtime error due to findstr.exe, remove findstr,
+for /f "delims=" %%i in (%rfilename%) do (
+::   echo.%%i | findstr /v "^ECHO" >> %WF%
    echo %%i >> %WF%
 )
+del %rfilename%
 echo ----- : end product : ----- >> %WF%
-echo ----- : end of script : ----- >> %WF%
+echo ----- : end result from script : ----- >> %WF%
--- var/common/mergelocalrunresult.py
+++ var/common/mergelocalrunresult.py
@@ -20,18 +20,40 @@ class MERGE_LOCAL_RUN_RESULT:
                   matchedlist.append(fname)
         return matchedlist
 
-    def parseLinuxOrigin(self, WORKENV):
-        print "run"
+    def parseWindowOrigin(self, targethost, WORKENV):
+        fpath = WORKENV['LOCALRUNUTILSRCPATH']+"/"+targethost
+        # read files
+        f = open(fpath, 'r')
+        msglist = f.readlines()
+        f.close()
+        # computersystem search
+        indexcount = 0
+        beginindex = 0
+        endindex = len(msglist)
+        for rline in msglist:
+            if re.compile(": begin computersystem :",re.I).search(rline):
+               beginindex = indexcount
+               continue
+            if re.compile(": end computersystem :",re.I).search(rline):
+               endindex = indexcount
+               continue
+            indexcount = indexcount + 1
+        print len(msglist[beginindex+1:endindex])
+        print len(msglist[beginindex+1].split())
+        print msglist[endindex-1]
+        print len(msglist[endindex-1].split())
+        sys.exit()
+        
 
     def runCmd(self, WORKENV):
         filenameslist = os.listdir(WORKENV['LOCALRUNUTILSRCPATH'])
         matchedlinuxlist = self.conditionmatchfilelist(filenameslist, re.compile("linux-", re.I), WORKENV['LOCALRUNUTILSRCPATH'])
         matchedwindowlist = self.conditionmatchfilelist(filenameslist, re.compile("window-", re.I), WORKENV['LOCALRUNUTILSRCPATH'])
         pList = []
-        for targets in matchedlinuxlist:
+        for targethost in matchedlinuxlist:
             pass
-        for targets in matchedwindowlist:
-            p = Process(target=self.parseLinuxOrigin, args=(WORKENV,))
+        for targethost in matchedwindowlist:
+            p = Process(target=self.parseWindowOrigin, args=(targethost, WORKENV,))
             p.start()
             pList.append(p)
         for p in pList:
