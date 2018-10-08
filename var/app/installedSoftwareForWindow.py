a35a23a6f31b959963a06482c56f19215225240a
Your Name <you@example.com>
2018-10-08 08:17:56 +0000

    update

--- var/app/installedSoftwareForWindow.py
+++ var/app/installedSoftwareForWindow.py
@@ -53,7 +53,8 @@ class InstalledSoftwareForWindow:
            fixed_rdictkey = fixed_rdict.keys()
            for kname in fixed_rdictkey:
                if re.compile("^caption",re.I).search(kname) and len(kname) == 7:
-                  tmpdict["caption"] = fixed_rdict[kname].strip()
+                  tmpdict["softwarename"] = fixed_rdict[kname].strip()
+                  #tmpdict["caption"] = fixed_rdict[kname].strip()
                   continue
                elif re.compile("^version",re.I).search(kname) and len(kname) == 7:
                   tmpdict["version"] = fixed_rdict[kname].strip()
