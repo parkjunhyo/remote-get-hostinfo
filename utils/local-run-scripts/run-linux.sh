c85cd32d76e3fb9f9c65a0aefd989adbd5d29400
Your Name <you@example.com>
2018-10-12 18:58:02 +0000

    update

--- utils/local-run-scripts/run-linux.sh
+++ utils/local-run-scripts/run-linux.sh
@@ -1,53 +1,128 @@
 #! /bin/bash
 
-# Get Local IP address
-#DEFAULTIF=$(route | grep -i "default" | awk '{print $8}')
+############ Command Validation-Checking ##################
+echo "Command Validation-Checking  .........."
+if [[ $(which hostnamectl) ]]
+then
+   HOSTCTLCMDSTATUS=1
+else
+   HOSTCTLCMDSTATUS=0
+fi
+echo ""
+########## GET LOCAL IP ADDRESS ########
+echo ""
 DEFAULTIF=$(ip route | grep -i default | awk '{print $5}')
 SEARCHEDIP=$(ip addr show $DEFAULTIF | grep -i 'inet ' | awk '{print $2}' | awk -F "[/]" '{print $1}')
 
-# Get Host Name
-SEARCHEDHOSTNAME=$(hostnamectl | grep -i "static hostname" | awk '{print $3}')
+############### GET HOST NAME ##########
+if [[ $HOSTCTLCMDSTATUS == 1 ]]
+then
+   SEARCHEDHOSTNAME=$(hostnamectl | grep -i "static hostname" | awk '{print $3}')
+else
+   SEARCHEDHOSTNAME=$(hostname)
+fi
 
-# File Path for Result
+######### CREATE FILE PATH TO WRITE THE RESULT ###############
 WF="linux-localrun-"$SEARCHEDHOSTNAME"-"$SEARCHEDIP".origin.txt"
 
-# Run Script
+######## RUN SCRIPT ########################
 echo "----- : begin result from script : -----" > $WF
-echo "hostnamectl info getting.."
+
+echo "hostnamectl (hostname) info getting.."
 echo "----- : begin hostnamectl : -----" >> $WF
-hostnamectl >> $WF
+if [[ $HOSTCTLCMDSTATUS == 1 ]]
+then
+   hostnamectl >> $WF
+else
+   echo Static hostname: $(hostname) >> $WF
+   ARCHSTR=$(uname -i)
+   LOWER_ARCHSTR=${ARCHSTR,,}
+   if [[ $LOWER_ARCHSTR =~ .*"unknown".* ]]
+   then
+      ARCHSTR=$(uname -m)
+   fi
+   echo Architecture: $ARCHSTR >> $WF
+fi
 echo "----- : end hostnamectl : -----" >> $WF
+
 
 echo "/etc/os-release info getting.."
 echo "----- : begin /etc/os-release : -----" >> $WF
-cat /etc/os-release >> $WF
+if [[ -f /etc/os-release ]]
+then
+   cat /etc/os-release >> $WF
+   #
+   TEMPSTATUS=$(cat /etc/os-release | grep -i id_like)
+   if [[ $TEMPSTATUS ]]
+   then
+      IDLIKE_TEMP=$(cat /etc/os-release | grep -i 'id_like=' | awk -F"[=]" '{print $2}')
+   else
+      IDLIKE_TEMP=$(cat /etc/os-release | grep -i 'id=' | awk -F"[=]" '{print $2}')
+   fi
+   IDLIKE_TEMP_LOWER=${IDLIKE_TEMP,,}
+else
+   if [[ -f /etc/system-release ]]
+   then
+      ONAME=$(cat /etc/system-release | awk '{if(NR==1) print $0}')
+      echo NAME=\"$ONAME\" >> $WF
+   elif [[ -f /etc/redhat-release ]]
+   then
+      ONAME=$(cat /etc/redhat-release | awk '{if(NR==1) print $0}')
+      echo NAME=\"$ONAME\" >> $WF
+   elif [[ -f /etc/centos-release ]]
+   then
+      ONAME=$(cat /etc/centos-release | awk '{if(NR==1) print $0}')
+      echo NAME=\"$ONAME\" >> $WF
+   elif [[ -f /etc/lsb-release ]]
+   then
+      ONAME=$(cat /etc/lsb-release | grep -i DISTRIB_DESCRIPTION | awk -F"[=]" '{if(NR==1) print $2}')
+      echo NAME=$ONAME >> $WF
+   else
+      ONAME=$(cat /etc/issue | awk '{if(NR==1) print $0}')
+      echo NAME=\"$ONAME\" >> $WF
+   fi
+   #
+   IDLIKE_TEMP=$(sudo find /etc/ -iname "*-release" | awk -F "[/]" '{if(NF==3) print $NF;}' | awk -F "[-]" '{printf $1" "}')
+   IDLIKE_TEMP_LOWER=${IDLIKE_TEMP,,}
+   echo ID_LIKE=$IDLIKE_TEMP_LOWER >> $WF
+   #
+   ID_TEMP=$(echo $ONAME | awk '{print $1$2}')
+   ID_TEMP_LOWER=${ID_TEMP,,}
+   echo ID=$ID_TEMP_LOWER >> $WF
+   #
+   VERSION_TEMP=$(echo $ONAME | awk -F"release" '{print $NF}')
+   echo VERSION=\"$VERSION_TEMP\" >> $WF
+   echo VERSION_ID=\"$VERSION_TEMP\" >> $WF
+fi
 echo "----- : end /etc/os-release : -----" >> $WF
 
+echo "kernal info getting.."
+echo "----- : begin kernal : -----" >> $WF
+KERNAL_TEMP=$(cat /proc/version | awk -F"[(]" '{print $1}')
+echo oskernal=$KERNAL_TEMP >> $WF
+echo "----- : end kernal : -----" >> $WF
 
-# GET Linux Distribution
-VALUECHECK=$(cat /etc/os-release | grep -i id_like)
-if [[ $VALUECHECK ]]
-then
-   ID_LIKE=$(cat /etc/os-release | grep -i 'id_like=' | awk -F"[=]" '{print $2}')
-else
-   ID_LIKE=$(cat /etc/os-release | grep -i 'id=' | awk -F"[=]" '{print $2}')
-fi
-#ID_LIKE=$(cat /etc/os-release | grep -i id_like | awk -F"[=]" '{print $2}')
 
-# Change all captial to lower case
-LOWERCASE_IDLIKE=${ID_LIKE,,}
-
-## Match Command and Run
 echo "product info getting.."
 echo "----- : begin product : -----" >> $WF
-if [[ $ID_LIKE =~ .*"debian".* ]] || [[ $ID_LIKE =~ .*"ubuntu".* ]]
-then
-  dpkg -l | awk '{print $2"!!!!!"$3}' >> $WF
-elif [[ $ID_LIKE =~ .*"rhel".* ]] || [[ $ID_LIKE =~ .*"fedora".* ]] || [[ $ID_LIKE =~ .*"centos".* ]] || [[ $ID_LIKE =~ .*"suse".* ]]
-then
-  rpm -qa >> $WF
-fi
+LOWERCASE_IDLIKE=${IDLIKE_TEMP_LOWER,,}
+IFS=' '
+read -ra VAL <<< $LOWERCASE_IDLIKE
+for i in "${VAL[@]}"; do
+    if [[ $i =~ .*"debian".* ]] || [[ $i =~ .*"ubuntu".* ]]
+    then
+       CMDTORUN="dpkg -l | awk '{print $2"!!!!!"$3}'"
+       break;
+    elif [[ $i =~ .*"rhel".* ]] || [[ $i =~ .*"redhat".* ]] || [[ $i =~ .*"fedora".* ]] || [[ $i =~ .*"centos".* ]] || [[ $i =~ .*"suse".* ]] || [[ $i =~ .*"sles".* ]]
+    then
+       CMDTORUN="rpm -qa"
+       break;
+    else
+       continue;
+    fi
+done
+$CMDTORUN >> $WF
 echo "----- : end product : -----" >> $WF
 
-# END
 echo "----- : end result from script : -----" >> $WF
+
